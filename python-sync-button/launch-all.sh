baf7b7dc720f3c3f239e3c29dd171e08b61c6b24
Luma Escalpelo <hugoescalpelo@gmail.com>
2025-03-25 06:57:45 -0600

    Update launch-all.sh

--- python-sync-button/launch-all.sh
+++ python-sync-button/launch-all.sh
@@ -4,12 +4,12 @@ echo "🚀 Iniciando todos los procesos en Leader..."
 cd /home/pione/Documents/GitHub/autoplayer/python-sync-button/
 
 echo "🎬 Ejecutando leader-sync.py"
-python3 leader_sync.py &
+python3 leader-sync.py &
 
 sleep 2
 
 echo "📡 Ejecutando leader-receiver.py"
-python3 leader_receiver.py &
+python3 leader-receiver.py &
 
 echo "🎛️ Ejecutando leader-buttons.py"
 python3 leader-buttons.py &
